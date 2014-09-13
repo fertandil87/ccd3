588d5acd679be2dba96db7d7a082c0ceeb4fa9d4
Fernando Saez <fertandil87@gmail.com>
2014-09-12 22:41:55 -0300

    updated icon

--- js/app.js
+++ js/app.js
@@ -8,14 +8,9 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers',
-  'mm.foundation',
-  'ezfb'
+  'mm.foundation'
 ]).
-config(['$routeProvider','ezfbProvider', function($routeProvider,ezfbProvider) {
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/:lugar/:tipo/:documento', {templateUrl: 'partials/documentos.html', controller: 'DocumentosCtrl'});
   $routeProvider.otherwise({redirectTo: '/serranostandil/principal/posiciones'});
-  
-  ezfbProvider.setInitParams({
-    appId: '386469651480295'
-  });
 }]);
